6d60e598683b11a538126905667a2a50d250ce85
suzushin54 <mkm.snicr@gmail.com>
2024-01-22 20:55:42 +0900

    refactor: Adjust form and input style and suggestion list positioning

--- web/src/components/molecules/SearchBar.tsx
+++ web/src/components/molecules/SearchBar.tsx
@@ -1,28 +1,48 @@
 "use client";
 
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
+import {useDebounce} from 'react-use';
 import Input from '@/components/atoms/Input';
-import Button from '@/components/atoms/Button';
 import {searchServerAction} from '@/actions/searchPhrase';
 import {useSearch} from '@/providers/SearchContext';
-
 import styles from './SearchBar.module.css';
 
 const SearchBar = () => {
-  const [suggestions, setSuggestions] = useState<string[]>([]);
-  const [query, setQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  // const [selectedSuggestion, setSelectedSuggestion] = useState<SelectOption | null>(null);
+  const [isFetching, setIsFetching] = useState(false);
+  const [suggestionOptions, setSuggestionOptions] = useState([]);
   const {setPhrases} = useSearch();
 
-  useEffect(() => {
-    if (query.length > 0 ) {
-      fetch(`http:///localhost:4000/phrases/suggest?q=${encodeURIComponent(query)}`)
-        .then(response => response.json())
-        .then(data => setSuggestions(data))
-        .catch(error => console.log('Error fetching suggestions: ', error));
-    } else {
-      setSuggestions([]);
-    }
-  }, [query]);
+  useDebounce(
+    () => {
+      if (searchQuery) {
+        setIsFetching(true);
+        fetch(`http:///localhost:4000/phrases/suggest?q=${encodeURIComponent(searchQuery)}`)
+          .then(response => response.json())
+          .then(data => {
+            setSuggestionOptions(data);
+            setIsFetching(false);
+          })
+          .catch(() => {
+            setIsFetching(false);
+          });
+      } else {
+        setSuggestionOptions([]);
+      }
+    },
+    200,
+    [searchQuery]
+  );
+
+  const handleInputChange = (newValue: string) => {
+    setSearchQuery(newValue);
+  };
+
+  // const handleChange = (selectedOption: SelectOption | null) => {
+  //   setSelectedSuggestion(selectedOption);
+  //   setSearchQuery(selectedOption ? selectedOption.value : '');
+  // };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -36,17 +56,18 @@ const SearchBar = () => {
       <form onSubmit={handleSubmit}>
         <Input
           className={styles.searchInput}
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          value={searchQuery}
+          onChange={(e) => handleInputChange(e.target.value)}
           name="query"
+          placeholder="Search..."
         />
-        <Button className={styles.searchButton} text="Search" />
-        {suggestions.length > 0 && (
-          <div className={styles.suggestions}>
-            {suggestions.map((suggestion, index) => (
-              <div key={index}>{suggestion}</div>
+        {isFetching && <div>Loading...</div>}
+        {suggestionOptions.length > 0 && (
+          <ul className={styles.suggestionsList}>
+            {suggestionOptions.map((suggestion, index) => (
+              <li key={index}>{suggestion}</li>
             ))}
-          </div>
+          </ul>
         )}
       </form>
     </div>
