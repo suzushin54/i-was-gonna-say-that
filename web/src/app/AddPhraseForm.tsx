9325228655068d71d3439fb649c1e0aeae096663
Shinichiro Mikami (Suzuki) <mkm.snicr@gmail.com>
2024-02-02 22:20:34 +0900

    feat: Add new phrase structure (#3)
    * fix: Apply AddPhraseForm to new phrase structure
    
    * feat: CRUD for tags
    
    * feat: Tag selectable using react-select

--- web/src/app/AddPhraseForm.tsx
+++ web/src/app/AddPhraseForm.tsx
@@ -1,9 +1,37 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import Select, { ActionMeta, MultiValue, StylesConfig } from 'react-select';
 import Button from '@/components/atoms/Button';
+import { Tag } from '@/types/tag'
 import styles from './AddPhraseForm.module.css';
 
 const AddPhraseForm: React.FC = () => {
-  const [phraseData, setPhraseData] = useState({scene: '', phrase: '', tags: ''});
+  const [registeredTags, setRegisteredTags] = useState<Tag[]>([]);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const tagOptions = registeredTags.map(tag => ({
+    value: tag.id.toString(),
+    label: tag.tag,
+  }));
+
+  useEffect(() => {
+    const fetchTags = async () => {
+      try {
+        const response = await fetch('http://localhost:4000/tags');
+        const registeredTag: Tag[] = await response.json();
+        setRegisteredTags(registeredTag);
+      } catch (error) {
+        console.error('Failed to fetch tags:', error);
+      }
+    };
+    fetchTags();
+  }, []);
+
+
+  const [phraseData, setPhraseData] = useState({
+    sceneName: '',
+    phrase: '',
+    japaneseTranslation: '',
+    tags: ''
+  });
 
   const handleSubmit = async () => {
     try {
@@ -13,8 +41,9 @@ const AddPhraseForm: React.FC = () => {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          scene: phraseData.scene,
+          sceneName: phraseData.sceneName,
           phrase: phraseData.phrase,
+          japaneseTranslation: phraseData.japaneseTranslation,
           tags: phraseData.tags.split(',').map(tag => tag.trim())
         })
       });
@@ -29,8 +58,41 @@ const AddPhraseForm: React.FC = () => {
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { id, value } = event.target as HTMLInputElement | HTMLTextAreaElement;
-    setPhraseData({ ...phraseData, [id]: value });
+    const {id, value} = event.target as HTMLInputElement | HTMLTextAreaElement;
+    setPhraseData({...phraseData, [id]: value});
+  };
+
+  interface OptionType {
+    value: string;
+    label: string;
+  }
+
+  const handleSelectChange = (
+    newValue: MultiValue<OptionType>,
+    actionMeta: ActionMeta<OptionType>
+  ) => {
+    const newSelectedTags = newValue.map(option => ({
+      id: parseInt(option.value, 10),
+      tag: option.label,
+    }));
+    setSelectedTags(newSelectedTags);
+  };
+
+  const selectedTagOptions = selectedTags.map(tag => ({
+    value: tag.id.toString(),
+    label: tag.tag,
+  }));
+
+  const selectStyles: StylesConfig<OptionType, boolean> = {
+    control: (base, state) => ({
+      ...base,
+      backgroundColor: '#262626',
+      borderColor: state.isFocused ? '#0044CC' : '#333333',
+      color: '#DDDDDD',
+      width: '100%',
+      borderRadius: '4px',
+      padding: '8px',
+    }),
   };
 
   return (
@@ -42,7 +104,7 @@ const AddPhraseForm: React.FC = () => {
           type="text"
           id="scene"
           className={styles.input}
-          value={phraseData.scene}
+          value={phraseData.sceneName}
           onChange={handleInputChange}
         />
       </div>
@@ -57,13 +119,23 @@ const AddPhraseForm: React.FC = () => {
         />
       </div>
       <div className={styles.formGroup}>
-        <label htmlFor="tags" className={styles.label}>Tags:</label>
-        <input
-          type="text"
-          id="tags"
-          className={styles.input}
-          value={phraseData.tags}
+        <label htmlFor="japaneseTranslation" className={styles.label}>Japanese Translation:</label>
+        <textarea
+          id="japaneseTranslation"
+          className={styles.textarea}
+          rows={3}
+          value={phraseData.japaneseTranslation}
           onChange={handleInputChange}
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor="tags" className={styles.label}>Tags:</label>
+        <Select
+          styles={selectStyles}
+          isMulti
+          options={tagOptions}
+          value={selectedTagOptions}
+          onChange={handleSelectChange}
         />
       </div>
       <Button text="Save" onClick={handleSubmit} variant="primary" />
