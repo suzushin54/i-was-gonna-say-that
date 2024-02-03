c8923c063f113b656c07065bff5e66db5fa7cae8
suzushin54 <mkm.snicr@gmail.com>
2024-02-03 13:14:18 +0900

    feat: TagSelect as a module, fix style for dark theme.

--- web/src/app/AddPhraseForm.tsx
+++ web/src/app/AddPhraseForm.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Select, { ActionMeta, MultiValue, StylesConfig } from 'react-select';
+import TagSelect from '@/components/molecules/TagSelect';
 import Button from '@/components/atoms/Button';
 import { Tag } from '@/types/tag'
 import styles from './AddPhraseForm.module.css';
@@ -7,10 +7,6 @@ import styles from './AddPhraseForm.module.css';
 const AddPhraseForm: React.FC = () => {
   const [registeredTags, setRegisteredTags] = useState<Tag[]>([]);
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
-  const tagOptions = registeredTags.map(tag => ({
-    value: tag.id.toString(),
-    label: tag.tag,
-  }));
 
   useEffect(() => {
     const fetchTags = async () => {
@@ -62,39 +58,6 @@ const AddPhraseForm: React.FC = () => {
     setPhraseData({...phraseData, [id]: value});
   };
 
-  interface OptionType {
-    value: string;
-    label: string;
-  }
-
-  const handleSelectChange = (
-    newValue: MultiValue<OptionType>,
-    actionMeta: ActionMeta<OptionType>
-  ) => {
-    const newSelectedTags = newValue.map(option => ({
-      id: parseInt(option.value, 10),
-      tag: option.label,
-    }));
-    setSelectedTags(newSelectedTags);
-  };
-
-  const selectedTagOptions = selectedTags.map(tag => ({
-    value: tag.id.toString(),
-    label: tag.tag,
-  }));
-
-  const selectStyles: StylesConfig<OptionType, boolean> = {
-    control: (base, state) => ({
-      ...base,
-      backgroundColor: '#262626',
-      borderColor: state.isFocused ? '#0044CC' : '#333333',
-      color: '#DDDDDD',
-      width: '100%',
-      borderRadius: '4px',
-      padding: '8px',
-    }),
-  };
-
   return (
     <form>
       <h2>New Phrase</h2>
@@ -106,6 +69,14 @@ const AddPhraseForm: React.FC = () => {
           className={styles.input}
           value={phraseData.sceneName}
           onChange={handleInputChange}
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor="tags" className={styles.label}>Tags:</label>
+        <TagSelect
+          registeredTags={registeredTags}
+          selectedTags={selectedTags}
+          setSelectedTags={setSelectedTags}
         />
       </div>
       <div className={styles.formGroup}>
@@ -126,16 +97,6 @@ const AddPhraseForm: React.FC = () => {
           rows={3}
           value={phraseData.japaneseTranslation}
           onChange={handleInputChange}
-        />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="tags" className={styles.label}>Tags:</label>
-        <Select
-          styles={selectStyles}
-          isMulti
-          options={tagOptions}
-          value={selectedTagOptions}
-          onChange={handleSelectChange}
         />
       </div>
       <Button text="Save" onClick={handleSubmit} variant="primary" />
