b0deaf4d17a986da3b860b87e8b93df2ee40aad3
suzushin54 <mkm.snicr@gmail.com>
2024-02-10 18:51:05 +0900

    feat: modal close button with mui

--- web/src/components/molecules/Modal.tsx
+++ web/src/components/molecules/Modal.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 import styles from './Modal.module.css';
 
 interface ModalComponentProps {
@@ -8,12 +10,30 @@ interface ModalComponentProps {
   children: React.ReactNode;
 }
 
-const ModalComponent: React.FC<ModalComponentProps> = ({isOpen, onClose, children}) => {
+const ModalComponent: React.FC<ModalComponentProps> = (
+  {isOpen, onClose, children}
+) => {
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={e => e.stopPropagation()}>
+        <IconButton
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: '15px',
+            top: '15px',
+            backgroundColor: 'transparent',
+            color: 'white',
+            '&:hover': {
+              backgroundColor: '#b00224',
+            },
+          }}
+          size="small"
+        >
+          <CloseIcon sx={{ fontSize: '16px' }} />
+        </IconButton>
         {children}
       </div>
     </div>,
