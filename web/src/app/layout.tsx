a46a9cd5731a61b3aa8e2ef51ad19718f9f22015
suzushin54 <mkm.snicr@gmail.com>
2024-02-13 22:23:49 +0900

    fix some styles

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,8 +3,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export const metadata: Metadata = {
   title: 'I was gonna say that - English Phrase Manager',
   description: 'Manage and learn common English phrases for various everyday situations.',
@@ -17,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
